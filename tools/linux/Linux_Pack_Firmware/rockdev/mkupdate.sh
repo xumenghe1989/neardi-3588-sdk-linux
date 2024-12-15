6b4a4861bbce07c6e84ec76008af924c349bb453
xumenghe1989 <245550828@qq.com>
2024-12-15 20:15:52 +0800

    Update mkupdate.sh

--- tools/linux/Linux_Pack_Firmware/rockdev/mkupdate.sh
+++ tools/linux/Linux_Pack_Firmware/rockdev/mkupdate.sh
@@ -2,14 +2,14 @@
 echo "start to make update.img..."
 if [ ! -f "Image/parameter" -a ! -f "Image/parameter.txt" ]; then
 	echo "Error:No found parameter!"
-	exit 1
+	exit 11
 fi
 if [ ! -f "package-file" ]; then
 	echo "Error:No found package-file!"
-	exit 1
+	exit 22
 fi
 ./afptool -pack ./ Image/update.img || pause
-./rkImageMaker -RK3588 Image/MiniLoaderAll.bin Image/update.img update.img -os_type:androidos || pause
+./rkImageMaker -RK3588 Image/MiniLoaderAll.bin Image/update.img update.img -os_type:androidos
 echo "Making ./Image/update.img OK."
 #echo "Press any key to quit:"
 #read -n1 -s key
